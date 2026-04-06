9308287f8426abc0991e625574af3606e8818629
Saiedf <saiedf@gmail.com>
2026-04-06 16:34:24 +0200

    UPDATE

--- VER 5.1/installer_theweatheriet5_5_1.sh
+++ VER 5.1/installer_theweatheriet5_5_1.sh
@@ -8,10 +8,10 @@
 # correct package for Dreambox and open-source Enigma2 images.
 #
 # Telnet command:
-# wget -O - "https://raw.githubusercontent.com/Saiedf/TheWeatherIet5/main/VER%205.1/installer_theweatheriet5_5_2.sh" | /bin/sh
+# wget -O - "https://raw.githubusercontent.com/Saiedf/TheWeatherIet5/main/VER%205.1/installer_theweatheriet5_5_1.sh" | /bin/sh
 #
 # Alternative command (download then run):
-# wget -O /tmp/installer_theweatheriet5_5_2.sh "https://raw.githubusercontent.com/Saiedf/TheWeatherIet5/main/VER%205.1/installer_theweatheriet5_5_2.sh" && chmod 755 /tmp/installer_theweatheriet5_5_2.sh && /bin/sh /tmp/installer_theweatheriet5_5_2.sh
+# wget -O /tmp/installer_theweatheriet5_5_1.sh "https://raw.githubusercontent.com/Saiedf/TheWeatherIet5/main/VER%205.1/installer_theweatheriet5_5_1.sh" && chmod 755 /tmp/installer_theweatheriet5_5_1.sh && /bin/sh /tmp/installer_theweatheriet5_5_1.sh
 # ==========================================================
 
 ########################################################################################################################
